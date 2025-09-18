e8e045dcf884a3ce9d8dec507741cb79f8e0e1e4
Naba Yousuf <nabay0519@gmail.com>
2025-09-18 19:05:11 +0500

    insert multiple documents

--- index.js
+++ index.js
@@ -35,17 +35,26 @@ const User = mongoose.model("User" ,userSchema);
 //   email: "naba@yahoo.in",
 //   age:18,
 // });
-const user2 = new User({
-  name: "Yousuf",
-  email: "ya@yahoo.in",
-  age:18,
-});
+// const user2 = new User({
+//   name: "Yousuf",
+//   email: "ya@yahoo.in",
+//   age:18,
+// });
 
 // user1.save(); 
-user2.save().then((res)=>{
-  console.log(res);
-}).catch((err)=>{
-  console.log(err);
-});
+// user2.save().then((res)=>{
+//   console.log(res);
+// }).catch((err)=>{
+//   console.log(err);
+// });
 // ye save function bhi async function hai , jo ki promise return karta hai , so we can use .then and .catch with it
 // user1.save().then(()=>{console.log("data saved")}).catch((err)=>{console.log(err)});
+
+//insert Multiple
+User.insertMany([
+  {name:"tony" , email:"tony@mail.com" , age:24},
+  {name:"shark",email:"tony@mail.com", age:19},
+  {name:"bruce",email:"bruce@mail.com", age:19},
+]).then((res)=>{
+  console.log(res)
+});
